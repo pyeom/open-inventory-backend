fce9b0b79698e6850e972b34301aa25b2716bd4a
Pipe <felipe.rojass@sansano.usm.cl>
2024-04-09 19:02:38 -0400

    some snippets added

--- open_inventory_backend/urls.py
+++ open_inventory_backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI, Schema
 from open_inventory_backend.models import *
+from .models import Prestamo
 #from open_inventory_backend import utils
 
 api = NinjaAPI()
@@ -26,6 +27,46 @@ api = NinjaAPI()
 def hello(request):
     return "Hello world!"
 
+""" @api.get("/inventory/")
+def get_inventory(request):
+    # Your logic to retrieve inventory items goes here
+    # Return a list of inventory items
+    return {"message": "List of inventory items"}
+ """
+
+""" @api.post("/prestar-item/")
+def crear_prestamo(request):
+    item_id = request.data.get('item_id')
+    borrower_id = request.data.get('borrower_id')
+    fecha_prestamo = request.data.get('fecha_prestamo')
+    is_paid = request.data.get('is_paid')
+    fee = request.data.get('fee')
+
+    # Your logic to create the lending transaction goes here
+    # Example: Create a new LendingTransaction object in the database
+
+    # Return a success message or relevant data
+    return {"message": "Lending transaction created successfully"}
+ """
+""" @api.get("/prestar-item")
+def get_lending_transactions(request):
+    # Your logic to retrieve lending transactions goes here
+    # Return a list of lending transactions
+    return {"message": "List of lending transactions"}
+ """
+""" @api.post("/prestar-item/{prestamo-id}")
+def devolver_item(request, prestamo_id: int):
+    # Your logic to mark the lending transaction as returned goes here
+    # Example: Update the LendingTransaction object in the database
+
+    # Return a success message or relevant data
+    return {"message": "Item returned successfully"}
+ """
+""" @api.get("/prestamos/")
+def get_all_transactions(request):
+    transactions = Prestamo.objects.all()
+    # You can customize the data returned (e.g., select specific fields)
+    return {"transactions": transactions} """
 
 urlpatterns = [
     path('admin/', admin.site.urls),
